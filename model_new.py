424907c389e7513948c60f57d677906d67b672f0
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-04-13 22:37:30 +0100

    Completed new code

--- model_new.py
+++ model_new.py
@@ -50,4 +50,38 @@ class Network:
         activation = x
         activations = [x]
 
-        
+        zs =[]
+        for b, w in zip(self.biases, self.weights):
+            z = np.dot(w, activation) + b
+            zs.append(z)
+            activation = sigmoid(z)
+            activations.append(activation)
+            
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+        nabla_b[-1] = delta
+        nabla_w[-1] = np.dot(delta, activations[-2].transpose())
+
+        for l in range(2, self.num_layers):
+            z = zs[-l]
+            sp = sigmoid_prime(z)
+            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            nabla_b[-l] = delta
+            nabla_w[-l] = np.dot(delta, activations[-l+1].transpose())
+
+        return (nabla_b, nabla_w)
+    
+
+    def evaluate(self, test_data):
+        test_results = [(np.argmax(self.feedforward(x)), y) for (x,y) in test_data]
+        return sum(int(x==y) for (x,y) in test_results)
+    
+    def cost_derivative(self, output_activations, y):
+        return (output_activations - y)
+    
+
+def sigmoid(z):
+    return 1.0/(1.0 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    return sigmoid(z)*(1-sigmoid(z))
