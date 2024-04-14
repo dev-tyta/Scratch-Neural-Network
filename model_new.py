1e64b521358b781343d9385118e09d03564cc8f7
Testimony Adekoya <hardeykoryar7@gmail.com>
2024-04-14 18:35:34 +0100

    New stuffs

--- model_new.py
+++ model_new.py
@@ -1,35 +1,30 @@
+#### Libraries
+# Standard library
 import random
+import string
+import binascii
 import numpy as np
 
+class NeuralNetwork:    
 
-class NeuralNetwork:
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.sizes = sizes
-        self.biases = [np.random.randn(y,1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y,x) for x,y in zip(sizes[:-1], sizes[1:])]
-
+        self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
     def feedforward(self, a):
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w,a)+ b)
+            a = sigmoid(np.dot(w, a)+b)
         return a
-    
 
-    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
-        if test_data:
-            n_test = len(test_data)
+    def SGD(self, training_data, epochs, mini_batch_size, eta):
         n = len(training_data)
-        for i in range(epochs):
+        for j in range(epochs):
             random.shuffle(training_data)
             mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, eta)
-            if test_data:
-                return f"Epoch {i}: {self.evaluate(test_data)}, {n_test}"
-            else:
-                return f"Epoch {i} complete."
-
 
     def update_mini_batch(self, mini_batch, eta):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
@@ -38,50 +33,53 @@ class NeuralNetwork:
             delta_nabla_b, delta_nabla_w = self.backprop(x, y)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
-
         self.weights = [w-(eta/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
-        self.biases = [b-(eta/len(mini_batch))* nb for b, nb in zip(self.biases, nabla_b)]
-
+        self.biases = [b-(eta/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
 
     def backprop(self, x, y):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-
+        # feedforward
         activation = x
-        activations = [x]
-
-        zs =[]
+        activations = [x] # list to store all the activations, layer by layer
+        zs = [] # list to store all the z vectors, layer by layer
         for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation) + b
+            z = np.dot(w, activation)+b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-            
+        # backward pass
+        # (actual out - desired out)*(derivative of potential)
         delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-
         for l in range(2, self.num_layers):
             z = zs[-l]
             sp = sigmoid_prime(z)
             delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
             nabla_b[-l] = delta
-            nabla_w[-l] = np.dot(delta, activations[-l+1].transpose())
-
+            nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
         return (nabla_b, nabla_w)
-    
 
     def evaluate(self, test_data):
-        test_results = [(np.argmax(self.feedforward(x)), y) for (x,y) in test_data]
-        return sum(int(x==y) for (x,y) in test_results)
-    
+        test_results = [(self.test_against_threshold(x, 0.9), np.argmax(y)) for (x, y) in test_data]
+        return sum(int(x == y) for (x, y) in test_results)
+
+    def test_against_threshold(self, vector, threshold):
+        out = [item for sublist in self.feedforward(vector).tolist() for item in sublist]
+        s = sum(out)
+        m = max(out)
+        index = -1
+        if (m > threshold):
+            index = out.index(m)
+        return index
+
     def cost_derivative(self, output_activations, y):
-        return (output_activations - y)
-    
-
+        return (output_activations-y)   
+        
+# auxiliary math functions
 def sigmoid(z):
-    return 1.0/(1.0 + np.exp(-z))
-
+        return 1.0/(1.0+np.exp(-z))
 
 def sigmoid_prime(z):
-    return sigmoid(z)*(1-sigmoid(z))
+        return sigmoid(z)*(1-sigmoid(z))
